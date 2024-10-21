019323bf64400b0d16a7ad4d4c84ffb288f3aa97
Gach1koi <k1ss.sakura@outlook.com>
2024-10-21 23:57:21 +0800

    1

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import { log } from 'console';
 import express from 'express';
 import { JsonDB, Config } from 'node-json-db';
+import { nanoid } from 'nanoid';
 import { fileURLToPath } from 'url';
+import formidable from 'formidable';
 const __filename = fileURLToPath(import.meta.url);
 const app = express();
 //必须放在前面，否则因为请求json数据的路径也是/members，images/members下的静态资源将无法响应
@@ -38,10 +40,33 @@ app.get('/members/:key', async (req, res) => {
         res.status(500).send('500 Internal Server Error');
     }
 });
+//响应前端请求的Activity.vue内的图片
 app.get('/activities', async (req, res) => {
     const db = new JsonDB(new Config("./data/json/activities.json", true, true, '/'));
     try {
         res.json(await db.getData(`/offline`));
+    }
+    catch (_a) {
+        res.status(500).send('500 Internal Server Error');
+    }
+});
+app.post('/register', async (req, res) => {
+    const form = formidable({
+        uploadDir: __filename + '/../data/images/avatars/',
+        keepExtensions: true
+    });
+    try {
+        //不需要错误处理也要加上前面的'_,'，因为js没有根据对应参数名传值的机制
+        form.parse(req, async (_, fields, files) => {
+            const id = nanoid();
+            const db = new JsonDB(new Config("./data/json/users.json", true, true, '/'));
+            log(fields.other, files);
+            db.push('/users[]', {
+                id,
+                avatarPath: files.avatar[0].filepath.split('/images')[1]
+            });
+            res.status(200).send('OK');
+        });
     }
     catch (_a) {
         res.status(500).send('500 Internal Server Error');
--- app.ts
+++ app.ts
@@ -2,13 +2,13 @@ import { log } from 'console'
 import express from 'express'
 import { JsonDB, Config } from 'node-json-db'
 import { nanoid } from 'nanoid';
-import path from 'path'
+import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
+import formidable from 'formidable';
 
 const __filename = fileURLToPath(import.meta.url)
-
-
 const app = express()
+
 
 //必须放在前面，否则因为请求json数据的路径也是/members，images/members下的静态资源将无法响应
 app.use(express.static(__filename + '/../data/images'))
@@ -52,6 +52,7 @@ app.get('/members/:key', async (req, res) => {
   }
 })
 
+//响应前端请求的Activity.vue内的图片
 app.get('/activities', async (req, res) => {
   const db = new JsonDB(new Config("./data/json/activities.json", true, true, '/'));
   try {
@@ -61,9 +62,27 @@ app.get('/activities', async (req, res) => {
   }
 })
 
-
-
-
+app.post('/register', async (req, res) => {
+  const form = formidable({
+    uploadDir: __filename + '/../data/images/avatars/',
+    keepExtensions: true
+  })
+  try {
+    //不需要错误处理也要加上前面的'_,'，因为js没有根据对应参数名传值的机制
+    form.parse(req, async (_, fields, files) => {
+      const id = nanoid()
+      const db = new JsonDB(new Config("./data/json/users.json", true, true, '/'))
+      log(fields.other,files)
+      db.push('/users[]', {
+        id,
+        avatarPath:files.avatar![0].filepath.split('/images')[1]
+      })
+      res.status(200).send('OK')
+    })
+  } catch {
+    res.status(500).send('500 Internal Server Error')
+  }
+})
 
 app.listen('777', async () => {
   log('qidong!')
